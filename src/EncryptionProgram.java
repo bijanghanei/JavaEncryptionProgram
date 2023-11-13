e9d2c6befb7daa01e7bc9868d621b2b8b68f400f
bijanghanei <iambijanghanei@gmail.com>
2023-11-12 23:24:40 -0500

    necessary methods added to EncryptionProgram class

---
+++ src/EncryptionProgram.java
@@ -0,0 +1,20 @@
+public class EncryptionProgram {
+    EncryptionProgram(){
+
+    }
+    private void setKey(){
+
+    }
+    private void getKey(){
+
+    }
+    private void encrypt(){
+
+    }
+    private void decrypt(){
+
+    }
+    private void quit(){
+
+    }
+}
